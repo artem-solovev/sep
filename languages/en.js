a353e118a6de0e6f5dcb697042a668c36eeb1ce8
Artem Solovev <artem.solovev.lse@gmail.com>
2016-09-19 13:04:55 +0300

    Added translation to global section

--- languages/en.js
+++ languages/en.js
@@ -1,6 +1,17 @@
 var translationsEN = {
     GLOBAL: {
         CHANGE_TO_ENGLISH: "English",
-        CHANGE_TO_RUSSIAN: "Russian"
+        CHANGE_TO_RUSSIAN: "Russian",
+        JOURNAL: "SEP Journal",
+        TOGGLE_NAVIGATION: "Toggle navigation",
+        NAV_DASHBOARD: "Dashboard",
+        NAV_ADDARTICLE: "Add article",
+        NAV_NEWARTICLES: "New articles",
+        NAV_STATS: "Stats",
+        NAV_LOGIN: "Log in",
+        NAV_HELLOUSER: "Hello user",
+        NAV_SETTINGS: "Settings",
+        NAV_HELP: "Help",
+        NAV_LOGOUT: "Log out"
     }
 };
--- languages/ru.js
+++ languages/ru.js
@@ -1,6 +1,17 @@
 var translationsRU = {
     GLOBAL: {
         CHANGE_TO_ENGLISH: "Английский",
-        CHANGE_TO_RUSSIAN: "Русский"
+        CHANGE_TO_RUSSIAN: "Русский",
+        JOURNAL: "Журнал НОПГЗ",
+        TOGGLE_NAVIGATION: "Переключить навигацию",
+        NAV_DASHBOARD: "Кабинет",
+        NAV_ADDARTICLE: "Отправить статью",
+        NAV_NEWARTICLES: "Новые статьи",
+        NAV_STATS: "Статистика",
+        NAV_LOGIN: "Войти",
+        NAV_HELLOUSER: "Привет пользователь",
+        NAV_SETTINGS: "Настройки",
+        NAV_HELP: "Справка",
+        NAV_LOGOUT: "Выйти"
     }
 };
