762a78b1e0fea70108591a12d592e75cd677ea6f
Artem Solovev <artem.solovev.lse@gmail.com>
2016-09-23 17:09:03 +0300

    Added name and degree to user profile

--- controllers/profileCtrl.js
+++ controllers/profileCtrl.js
@@ -1,12 +1,21 @@
-app.controller( 'profileCtrl', [ 'authService',  function( authService ) {
+app.controller( 'profileCtrl', [ 'authService', function( authService ) {
 
     var self = this;
+
+    self.edit = {};
+    
+    self.userProfile = {};
 
     self.info = authService.getUserInfo();
 
     self.getInfo = function() {
-        console.log( self.userInfo );
-        console.warn( authService.getUserInfo() );
-    }
+        //console.log( self.info );
+        console.log( self.userProfile );
+    };
+    
+    self.submitForm = function( data ) {
+        authService.updateUserProfile( self.info.$id, data );
+        //self.info = authService.getUserInfo();
+    };
 
 } ] );
--- services/authService.js
+++ services/authService.js
@@ -1,4 +1,4 @@
-app.factory( 'authService',  [ '$firebaseAuth', '$window', function( $firebaseAuth, $window ) {
+app.factory( 'authService',  [ '$firebaseObject', '$firebaseAuth', '$window', function( $firebaseObject, $firebaseAuth, $window ) {
 
     var authService = {};
 
@@ -10,12 +10,17 @@ app.factory( 'authService',  [ '$firebaseAuth', '$window', function( $firebaseAu
 
     authService.authUser = function( email, password ) {
         auth.$signInWithEmailAndPassword( email, password ).then(function( user ) {
-            $window.location.href = "/";
+            authService.userExists( firebase.auth().currentUser.uid,
+                                   firebase.auth().currentUser.email,
+                                   firebase.auth().currentUser.emailVerified);
+            console.log( firebase.auth().currentUser );
+            //$window.location.href = "/";
         }, function( error ) {
             var errorCode = error.code;
             var errorMessage = error.message;
             console.info( "Error in authUser - errorCode: " + errorCode + ". errorMessage: " + errorMessage);
         });
+
     };
 
     authService.signOut = function() {
@@ -27,7 +32,42 @@ app.factory( 'authService',  [ '$firebaseAuth', '$window', function( $firebaseAu
     };
 
     authService.getUserInfo = function() {
-        return firebase.auth().currentUser;
+        var path = 'users/' + firebase.auth().currentUser.uid;;
+
+        var userRef = firebase.database().ref( path ) ;
+        var user = $firebaseObject( userRef );
+
+        console.log( user );
+
+        return user;
+    };
+
+    authService.userExists = function( uid, email, emailVerified ) {
+        var usersRef = firebase.database().ref( 'users' );
+
+        usersRef.once( "value", function( snap ) {
+            if( snap.exists() ) {
+                // User exists
+            } else {
+                authService.createUserNode( uid, email, emailVerified );
+            }
+        } );
+    };
+
+    authService.createUserNode = function( uid, email, emailVerified ) {
+        var usersRef = firebase.database().ref( 'users/' );
+
+        usersRef.child( uid ).set( {
+            email: email,
+            emailVerified: emailVerified
+        } );
+    };
+
+    authService.updateUserProfile = function( uid, data ) {
+        var usersRef = firebase.database().ref( 'users/' );
+
+        usersRef.child( uid ).update( data );
+
     };
 
     return authService;
