6ab5845eb723d05c5de67a34f0bff833429a84b0
Artem Solovev <artem.solovev.lse@gmail.com>
2016-09-26 14:46:52 +0300

    Added info panel and deleted some spaces

--- controllers/profileCtrl.js
+++ controllers/profileCtrl.js
@@ -3,7 +3,7 @@ app.controller( 'profileCtrl', [ 'authService', function( authService ) {
     var self = this;
 
     self.edit = {};
-    
+
     self.userProfile = {};
 
     self.info = authService.getUserInfo();
@@ -12,12 +12,12 @@ app.controller( 'profileCtrl', [ 'authService', function( authService ) {
         //console.log( self.info );
         console.log( self.userProfile );
     };
-    
+
     self.submitForm = function( data ) {
         authService.updateUserProfile( self.info.$id, data );
         //self.info = authService.getUserInfo();
     };
-    
+
     self.activateAccount = function() {
         authService.sendActivationEmail();
     };
